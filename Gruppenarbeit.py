c8fa0f32a6084451a00522ae53cc62a1ed3acbc2
jacometjoey-dotcom <jacometjoey@gmail.com>
2025-10-24 11:05:00 +0200

    small change

--- Gruppenarbeit.py
+++ Gruppenarbeit.py
@@ -5,8 +5,6 @@ from Feature_01 import prime_Numbers
 
 
 
-Test_List = [ i for i in range (1,100) ]
-
 
 
 write = st.write
@@ -17,4 +15,4 @@ write("# Gruppenarbeit")
 write("This is our Gruopprojekt, here should be all the work we do together.")
 print(" I switched write.st to write for easier typing ")
 
-write(prime_Numbers (Test_List))
+write(prime_Numbers (20))
