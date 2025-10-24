5e3c90a3171e88794887377bad4726cd7129f078
jacometjoey-dotcom <jacometjoey@gmail.com>
2025-10-24 10:16:00 +0200

    Changes

--- Gruppenarbeit.py
+++ Gruppenarbeit.py
@@ -1,1 +1,11 @@
-print("Hello World")
+import streamlit as st
+
+
+write = st.write
+
+
+
+write("# Gruppenarbeit")
+write("Dies ist eine Gruppenarbeit.")
+write("Jeder kann hier seinen Beitrag leisten.")
+
