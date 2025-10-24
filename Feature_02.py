d2ead067242358c5b5f5704515badac9ebf75d8b
jacometjoey-dotcom <jacometjoey@gmail.com>
2025-10-24 13:41:40 +0200

    change

--- Feature_02.py
+++ Feature_02.py
@@ -4,8 +4,3 @@ def even_numbers(n):
     """Return a list of even numbers up to n (exclusive)."""
     return [i for i in range(n) if i % 2 == 0]  
 
-
-
-def nuneven_numbers(n):
-    """Return a list of odd numbers up to n (exclusive)."""
-    return [i for i in range(n) if i % 2 != 0]  
