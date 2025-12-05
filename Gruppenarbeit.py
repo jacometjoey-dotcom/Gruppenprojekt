47605eca7de754d364e9d0ef69c169911bf09345
jacometjoey-dotcom <jacometjoey@gmail.com>
2025-12-05 17:06:58 +0100

    Fix markdown typo in Gruppenarbeit.py

--- Gruppenarbeit.py
+++ Gruppenarbeit.py
@@ -563,7 +563,7 @@ for prop in Properties:
 
             with spalte_facts:
                 with st.container (border=True):
-                    st.marktdown("Property Facts")
+                    st.markdown("Property Facts")
                     col_f1_j, col_f2_j = st.columns(2)
                     with col_f1_j:
                         st.markdown(f"**Price**<br>{prop['facts']['price']}", unsafe_allow_html=True)
