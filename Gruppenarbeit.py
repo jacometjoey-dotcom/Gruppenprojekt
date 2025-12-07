36892f3294f0bca9270c87ce8796a4f0e907bb0f
jacometjoey-dotcom <jacometjoey@gmail.com>
2025-12-07 23:53:03 +0100

    Enhance calculation accuracy with new factors
    Added a multiplier for more accurate calculations based on location and construction quality factors.

--- Gruppenarbeit.py
+++ Gruppenarbeit.py
@@ -430,6 +430,21 @@ map_data = pd.DataFrame(
     } for prop in Properties]
 )
 
+
+#added a multiplyer so the calculations are more accurate 
+
+
+#Facotr 1: Location & Land Factor, difference in median price per square meter (Iowa 1'800 vs zurich 8500) 
+factor_location_land = 4.8 
+
+#Factor 2: The Construction Quality (Material and Human resources cost)
+#construction method in USA "stick build" vs switzerland solid construction --> prox 60% more expensive 
+factor_construction_quality = 1.6 
+
+swiss_adjustment_factor = factor_location_land * factor_construction_quality
+
+
+
 #A: here i built a tooltip to show the property name and image when hovering over the dots on the map, so that a user can see more information about the property
 
 BASE_URL = "https://raw.githubusercontent.com/jacometjoey-dotcom/Gruppenprojekt/main/"
